eaa8cda727c51cdf216c35d47ccaa993e03c1016
ma_lin <ma_lin@csisecurity.com.cn>
2025-04-30 13:20:36 +0800

    push

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  publicDir: 'public', // 指定静态资源目录
-})
